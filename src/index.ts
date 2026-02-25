69dc172df59343f0cf778e201190c1b7a4ad5417
IsaqueTADS <isaqueofc18@gmail.com>
2026-02-24 23:46:46 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-console.log("Hello World = 1");
+import "dotenv/config";
 
+import Fastify from "fastify";
 
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+try {
+  await fastify.listen({ port: Number(process.env.PORT) || 3333 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
