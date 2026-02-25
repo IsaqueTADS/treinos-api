0b02d240a7a3970d7a11b2f23df9862ed7bb159b
IsaqueTADS <isaqueofc18@gmail.com>
2026-02-25 00:31:55 -0300

    chore: add fastify swagger generation

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,70 @@
 import "dotenv/config";
 
+import fastifySwagger from "@fastify/swagger";
+import ScalarApiReference from "@scalar/fastify-api-reference";
 import Fastify from "fastify";
 import type { ZodTypeProvider } from "fastify-type-provider-zod";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from "fastify-type-provider-zod";
 import z from "zod";
 
+const envToLogger = {
+  development: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
+      },
+    },
+  },
+  production: true,
+  test: false,
+};
+
 const app = Fastify({
-  logger: true,
+  logger: envToLogger["development"] ?? true,
 }).withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Treinos API",
+      description:
+        "API RESTful para gerenciamento de treinos, permitindo criação, edição, listagem e exclusão de treinos",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        description: "Localhost",
+        url: "http://localhost:3333/",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+ app.register(ScalarApiReference, {
+  routePrefix: "/docs",
+  configuration: {
+    theme: "bluePlanet",
+  },
+});
 
 app.get(
   "/",
@@ -34,7 +85,10 @@ app.get(
 );
 
 try {
-  await app.listen({ port: Number(process.env.PORT) || 3333 });
+  await app.listen({ port: Number(process.env.PORT) || 3333 }).then(() => {
+    console.log("🚀 HTTP server running on http://localhost:3333/");
+    console.log("📚 Docs available at http://localhost:3333/docs/");
+  });
 } catch (err) {
   app.log.error(err);
   process.exit(1);
